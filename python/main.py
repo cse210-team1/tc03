16cdc8fea296916490f58ef9d70ec6553cb84703
TJ <teetothejay13@gmail.com>
2021-06-10 16:17:45 -0600

    putnam.py added

--- python/main.py
+++ python/main.py
@@ -1,7 +1,9 @@
 from bloomfield import david_greeting
 from cloward import clowardGreeting
+from putnam import tj_print
 
 print("Welcome to our collaborative program")
 
 david_greeting()
 clowardGreeting()
+tj_print()
