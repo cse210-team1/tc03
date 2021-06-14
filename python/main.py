6e09d0f110d375f1d0c027a4b6291110c5bf8bf3
steven <sseller6@byui.edu>
2021-06-14 12:52:13 -0600

    This is Steven Completing the group project.

--- python/main.py
+++ python/main.py
@@ -1,5 +1,7 @@
 from bloomfield import david_greeting
-
+from sellers import sellers_greeting
 print("Welcome to our collaborative program")
 
 david_greeting()
+sellers_greeting()
+
---
+++ python/sellers.py
@@ -0,0 +1,4 @@
+#This is me completing step 4
+def sellers_greeting():
+    print('Hello all, this is Steven.')
+    
